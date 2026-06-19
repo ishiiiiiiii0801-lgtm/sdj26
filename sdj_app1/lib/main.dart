1ef9fc226e43340f00a92de9355517e92e2f0bdb
shion <e1924008@oit.ac.jp>
2026-06-19 18:07:18 +0900

    おい

--- sdj_app1/lib/main.dart
+++ sdj_app1/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'おい',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 111, 223, 223)),
       ),
